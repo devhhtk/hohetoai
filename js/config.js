96f2ef71ce5c12db5db7d571af24726763af547a
devhhtk <devhhtk@kymafoundry.com>
2026-03-28 21:04:21 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
  */
 const CONFIG = {
   // API_BASE_URL: 'https://aumage-pipeline.rk-pc.workers.dev', // Production
-  API_BASE_URL: 'http://localhost:8787', // Local Development (default for wrangler dev)
+  API_BASE_URL: 'https://hohetoai.vercel.app', // Local Development (default for wrangler dev)
 };
 
 window.CONFIG = CONFIG;
