c9f55cccfe0ec3f1fb36e7286ba89a29cf150d8d
devhhtk <devhhtk@kymafoundry.com>
2026-03-28 23:10:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
  * Aumage — Frontend Configuration
  */
 const CONFIG = {
-   API_BASE_URL: 'https://aumage-pipeline.rk-pc.workers.dev', // Production
+   API_BASE_URL: 'https://hohetai-back.devhhtk.workers.dev', // Production
 };
 
 window.CONFIG = CONFIG;
