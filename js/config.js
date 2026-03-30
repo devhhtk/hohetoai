ef88bff46acbb86293f2ca168ea2f18424d46ceb
sourodip <sourodip@ascinatetech.com>
2026-03-30 15:21:35 +0530

    api url

--- js/config.js
+++ js/config.js
@@ -3,6 +3,7 @@
  */
 const CONFIG = {
    API_BASE_URL: 'https://aumage-pipeline.admin-it-e6e.workers.dev', // Production
+   // API_BASE_URL: 'http://localhost:8787', // Local
 };
 
 window.CONFIG = CONFIG;
