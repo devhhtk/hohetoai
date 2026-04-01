e1814e784d5ed0a9b0359d489661d76f0838a75a
ascinate <35849551+ascinate@users.noreply.github.com>
2026-04-01 20:49:51 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
  */
 const CONFIG = {
    // API_BASE_URL: 'https://aumage-pipeline.admin-it-e6e.workers.dev', // Production
-    API_BASE_URL: 'https://hohetai-back.devhhtk.workers.dev', // Local (use IP to bypass IPv6 resolution issues)
+    API_BASE_URL: 'https://hohetai-api.devhhtk.workers.dev', // Local (use IP to bypass IPv6 resolution issues)
 };
 
 window.CONFIG = CONFIG;
