3d9309d6877c074b1feba87d82553d876dab9519
sourodip <sourodip@ascinatetech.com>
2026-04-10 17:00:31 +0530

    zoom fix2

--- js/card-compositor.js
+++ js/card-compositor.js
@@ -59,7 +59,7 @@ var AumageCard = {
       const vpw = data.vpw !== undefined ? data.vpw : 675;
       const vph = data.vph !== undefined ? data.vph : 500;
 
-      const vzoom = data.vzoom || 0.5;
+      const vzoom = data.vzoom || 0.8;
       const voffset = data.voffset || { x: 0, y: 0 };
 
       this._drawCoverImg(ctx, creatureImg, vpx, vpy, vpw, vph, vzoom, voffset);
